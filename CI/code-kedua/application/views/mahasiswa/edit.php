d0e6d43f62758688fafa6384c77ae05709621b57
agitariirawan <akuagit@yahoo.com>
2020-02-17 12:09:37 +0700

    LAPORAN CODE DAN JOBSHEET 1-4

---
+++ CI/code-kedua/application/views/mahasiswa/edit.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Form Merubah Data Mahasiswa
+                </div>
+                <div class="card-body">
+                    <?php if (validation_errors()) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?= validation_errors() ?>
+                        </div>
+                    <?php endif ?>
+                    <form action="<?= base_url('mahasiswa/tambah') ?>" method="post">
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" id="nama" name="nama" value="<?= $mahasiswa['nama']; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="nim">Nim</label>
+                            <input type="number" class="form-control" id="nim" name="nim" value="<?= $mahasiswa['nim']; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" value="<?= $mahasiswa['email']; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="jurusan">Jurusan</label>
+                            <select name="jurusan" id="jurusan" class="form-control">
+                                <option selected>------</option>
+                                <option value="Kimia">Kimia</option>
+                                <option value="Informatika">Informatika</option>
+                                <option value="Mesin">Mesin</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary float-right">Edit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
